ee01efe72053727ce3b8a97ccdf290f6ec6da2ea
Thvi-Sev <sevthvi@11gmail.com>
2024-08-15 09:17:31 +0700

    Happy file

---
+++ Funny2/script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question")
+const gif = document.querySelector(".gif")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "I love you too!";
+    gif.src =
+    "https://i.pinimg.com/originals/bc/0a/ff/bc0affc97f7ea9649ad19cf29f913901.gif"
+
+
+});
+
+noBtn.addEventListener('mouseover', ()=> {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + 'px';
+    noBtn.style.top = randomY + 'px';
+});
